1b4276d58fbcdd2d9c1cc3a516247d72021e6f10
Rune Tvilum <rune@addin.dk>
2013-03-21 14:06:48 +0100

    Initial release

---
+++ Form1.Designer.cs
@@ -0,0 +1,218 @@
+﻿namespace ASC2LandXML
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.Filter = "ESRI ASCII Grid (*.asc)|*.asc";
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 3;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tableLayoutPanel1.Controls.Add(this.button1, 2, 0);
+            this.tableLayoutPanel1.Controls.Add(this.textBox1, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.label1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.button2, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.textBox2, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.button3, 2, 1);
+            this.tableLayoutPanel1.Controls.Add(this.label2, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.progressBar1, 0, 3);
+            this.tableLayoutPanel1.Controls.Add(this.richTextBox1, 0, 4);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 5;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(472, 152);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button1.Location = new System.Drawing.Point(439, 3);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(30, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "...";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.DataBindings.Add(new System.Windows.Forms.Binding("Text", global::ASC2LandXML.Properties.Settings.Default, "input", true, System.Windows.Forms.DataSourceUpdateMode.OnPropertyChanged));
+            this.textBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textBox1.Location = new System.Drawing.Point(51, 3);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(382, 20);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.Text = global::ASC2LandXML.Properties.Settings.Default.input;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label1.Location = new System.Drawing.Point(3, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(42, 29);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Input:";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // button2
+            // 
+            this.tableLayoutPanel1.SetColumnSpan(this.button2, 3);
+            this.button2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button2.Location = new System.Drawing.Point(3, 61);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(466, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Start";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.DataBindings.Add(new System.Windows.Forms.Binding("Text", global::ASC2LandXML.Properties.Settings.Default, "output", true, System.Windows.Forms.DataSourceUpdateMode.OnPropertyChanged));
+            this.textBox2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textBox2.Location = new System.Drawing.Point(51, 32);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(382, 20);
+            this.textBox2.TabIndex = 5;
+            this.textBox2.Text = global::ASC2LandXML.Properties.Settings.Default.output;
+            // 
+            // button3
+            // 
+            this.button3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button3.Location = new System.Drawing.Point(439, 32);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(30, 23);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "...";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label2.Location = new System.Drawing.Point(3, 29);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(42, 29);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Output:";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.BackColor = System.Drawing.SystemColors.Control;
+            this.tableLayoutPanel1.SetColumnSpan(this.progressBar1, 3);
+            this.progressBar1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.progressBar1.Location = new System.Drawing.Point(3, 90);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(466, 14);
+            this.progressBar1.TabIndex = 8;
+            // 
+            // saveFileDialog1
+            // 
+            this.saveFileDialog1.Filter = "LandXML (*.xml)|*.xml";
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.WorkerReportsProgress = true;
+            this.backgroundWorker1.WorkerSupportsCancellation = true;
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            this.backgroundWorker1.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.backgroundWorker1_ProgressChanged);
+            this.backgroundWorker1.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.backgroundWorker1_RunWorkerCompleted);
+            // 
+            // richTextBox1
+            // 
+            this.tableLayoutPanel1.SetColumnSpan(this.richTextBox1, 3);
+            this.richTextBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.richTextBox1.Location = new System.Drawing.Point(3, 110);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(466, 39);
+            this.richTextBox1.TabIndex = 9;
+            this.richTextBox1.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(472, 152);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "Form1";
+            this.Text = "ASC2LandXML";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,222 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace ASC2LandXML
+{
+    public partial class Form1 : Form
+    {
+        string status = "";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            if (openFileDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                textBox1.Text = openFileDialog1.FileName;
+                Properties.Settings.Default.Save();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            status = "";
+            richTextBox1.Text = "";
+            progressBar1.ForeColor = Color.Blue;
+            progressBar1.Value = 0;
+            backgroundWorker1.RunWorkerAsync();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (saveFileDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                textBox2.Text = saveFileDialog1.FileName;
+                Properties.Settings.Default.Save();
+            }
+        }
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            try
+            {
+                SortedDictionary<int, int> points = new SortedDictionary<int, int>();
+                NumberFormatInfo nfi = new NumberFormatInfo();
+                nfi.NumberDecimalSeparator = ".";
+                nfi.NumberGroupSeparator = ",";
+                XmlTextWriter xmltextWriter = new XmlTextWriter(Properties.Settings.Default.output, Encoding.UTF8);
+                xmltextWriter.Formatting = Formatting.Indented;
+                xmltextWriter.WriteStartDocument();
+                xmltextWriter.WriteStartElement("LandXML");
+                xmltextWriter.WriteAttributeString("version", "1.0");
+                xmltextWriter.WriteAttributeString("date", "2012-11-12");
+                xmltextWriter.WriteAttributeString("time", "14:54:53");
+                xmltextWriter.WriteAttributeString("xmlns", "http://www.landxml.org/schema/LandXML-1.0");
+                xmltextWriter.WriteAttributeString("xmlns:xsi", "http://www.w3.org/2001/XMLSchema-instance");
+                xmltextWriter.WriteAttributeString("xsi:schemaLocation", "http://www.landxml.org/schema/LandXML-1.0 http://www.landxml.org/schema/landxml-1.0/LandXML-1.0.xsd");
+                xmltextWriter.WriteStartElement("Units");
+                xmltextWriter.WriteStartElement("Metric");
+                xmltextWriter.WriteAttributeString("areaUnit", "squareMeter");
+                xmltextWriter.WriteAttributeString("linearUnit", "meter");
+                xmltextWriter.WriteAttributeString("volumeUnit", "cubicMeter");
+                xmltextWriter.WriteAttributeString("temperatureUnit", "celsius");
+                xmltextWriter.WriteAttributeString("pressureUnit", "HPA");
+                xmltextWriter.WriteEndElement();//Metric
+                xmltextWriter.WriteEndElement();//Units
+                xmltextWriter.WriteStartElement("Application");
+                xmltextWriter.WriteAttributeString("name", "LandXML");
+                xmltextWriter.WriteAttributeString("manufacturer", "addin.dk");
+                xmltextWriter.WriteAttributeString("version", "1.0");
+                xmltextWriter.WriteAttributeString("manufacturerURL", "www.addin.dk");
+                xmltextWriter.WriteEndElement();//Application
+                xmltextWriter.WriteStartElement("Surfaces");
+                xmltextWriter.WriteStartElement("Surface");
+                xmltextWriter.WriteAttributeString("name", "DHM");
+                xmltextWriter.WriteAttributeString("desc", "DHM");
+                xmltextWriter.WriteAttributeString("state", "proposed");
+                xmltextWriter.WriteStartElement("Definition");
+                xmltextWriter.WriteAttributeString("surfType", "TIN");
+                xmltextWriter.WriteStartElement("Pnts");
+
+                using (StreamReader sr = new StreamReader(Properties.Settings.Default.input))
+                {
+                    string line = sr.ReadLine();
+                    string[] data = line.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+                    int ncols = 0;
+                    int.TryParse(data[1], out ncols);
+
+                    line = sr.ReadLine();
+                    data = line.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+                    int nrows = 0;
+                    int.TryParse(data[1], out nrows);
+
+                    line = sr.ReadLine();
+                    data = line.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+                    int xllcorner = 0;
+                    int.TryParse(data[1], NumberStyles.Number, nfi, out xllcorner);
+
+                    line = sr.ReadLine();
+                    data = line.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+                    int yllcorner = 0;
+                    int.TryParse(data[1], NumberStyles.Number, nfi, out yllcorner);
+
+                    line = sr.ReadLine();
+                    data = line.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+                    int cellsize = 0;
+                    int.TryParse(data[1], out cellsize);
+
+                    line = sr.ReadLine();
+                    data = line.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+                    double NODATA_value = 0;
+                    double.TryParse(data[1], out NODATA_value);
+
+                    int id = 0;
+                    int a = 0;
+                    while ((line = sr.ReadLine()) != null)
+                    {
+                        backgroundWorker1.ReportProgress((int)(100 * sr.BaseStream.Position / sr.BaseStream.Length));
+
+                        int y = yllcorner + nrows * cellsize - a * cellsize;
+                        a++;
+                        data = line.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+                        for (int i = 0; i < ncols; i++)
+                        {
+                            double z = 0;
+                            double.TryParse(data[i], out z);
+                            if (z != NODATA_value)
+                            {
+                                id++;
+                                int x = xllcorner + i * cellsize;
+
+                                xmltextWriter.WriteStartElement("P");
+                                xmltextWriter.WriteAttributeString("id", id.ToString());
+                                xmltextWriter.WriteString(y.ToString() + " " + x.ToString() + " " + data[i]);
+                                xmltextWriter.WriteEndElement();
+                                points.Add(id, 0);
+                            }
+                        }
+                    }
+
+                    xmltextWriter.WriteEndElement();//Pnts
+                    xmltextWriter.WriteStartElement("Faces");
+                    int k = 0;
+                    int total = (nrows-1) * (ncols-1);
+                    int lastprogress = 0;
+                    for (int i = 0; i < nrows - 1; i++)
+                    {
+                        for (int j = 1; j < ncols; j++)
+                        {
+                            k++;
+                            int progress = (int)(100 * (float)k / (float)total);
+                            if (progress != lastprogress)
+                            {
+                                backgroundWorker1.ReportProgress(progress);
+                                lastprogress = progress;
+                            }
+                            if (points.ContainsKey(i * ncols + j) && points.ContainsKey(i * ncols + j + 1) && points.ContainsKey(i * ncols + j + ncols))
+                            {
+                                xmltextWriter.WriteStartElement("F");
+                                xmltextWriter.WriteString((i * ncols + j).ToString() + " " + (i * ncols + j + 1).ToString() + " " + (i * ncols + j + ncols).ToString());
+                                xmltextWriter.WriteEndElement();
+                            }
+                            if (points.ContainsKey(i * ncols + j + ncols) && points.ContainsKey(i * ncols + j + 1) && points.ContainsKey(i * ncols + j + 1 + ncols))
+                            {
+                                xmltextWriter.WriteStartElement("F");
+                                xmltextWriter.WriteString((i * ncols + j + ncols).ToString() + " " + (i * ncols + j + 1).ToString() + " " + (i * ncols + j + 1 + ncols).ToString());
+                                xmltextWriter.WriteEndElement();
+                            }
+                        }
+                    }
+                    xmltextWriter.WriteEndElement();//Faces
+                    xmltextWriter.WriteEndElement();//Definition
+                    xmltextWriter.WriteStartElement("Feature");
+                    xmltextWriter.WriteAttributeString("code", "Surface");
+                    xmltextWriter.WriteStartElement("Property");
+                    xmltextWriter.WriteAttributeString("label", "pref");
+                    xmltextWriter.WriteAttributeString("value", "Default");
+                    xmltextWriter.WriteEndElement();//Property
+                    xmltextWriter.WriteStartElement("Property");
+                    xmltextWriter.WriteAttributeString("label", "guid");
+                    xmltextWriter.WriteAttributeString("value", "c83d3755-cece-4fdc-9bfe-d39c834dad1f");
+                    xmltextWriter.WriteEndElement();//Property
+                    xmltextWriter.WriteEndElement();//Feature
+
+                    xmltextWriter.WriteEndElement();//Surface
+                    xmltextWriter.WriteEndElement();//Surfaces
+                    xmltextWriter.WriteEndElement();//LandXML
+                }
+                xmltextWriter.Close();
+                status = "OK";
+            }
+            catch (Exception ex)
+            {
+                status= ex.Message+"\n"+ex.StackTrace;
+                backgroundWorker1.CancelAsync();
+            }
+        }
+
+        private void backgroundWorker1_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            progressBar1.Value = e.ProgressPercentage;
+//            progressBar1.Update();
+        }
+
+        private void backgroundWorker1_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            richTextBox1.Text = status;
+        }
+    }
+}
